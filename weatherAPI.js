8fbe43de3dae4c86a2f77316d7c9c612b222955c
Ridwan <106625392+Young-Alhaji@users.noreply.github.com>
2022-06-29 09:23:53 -0700

    Add files via upload
    A new logo was added and responsiveness feature was added for all users of different device sizes

--- weatherAPI.js
+++ weatherAPI.js
@@ -1,13 +1,16 @@
+
+
 async function forecast(){
 let cityName = cityname.value
 const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=09756d60b2011a99fb389f6e43e766cc&units=metric`
 let response = await fetch(url)
 let convertedResponse= await response.json()
-console.log(convertedResponse)
   locatio.innerHTML= `  ${convertedResponse.sys.country} <br> ${convertedResponse.name} `
   locatio2.innerHTML=` ${convertedResponse.weather[0].description}`
   maintemp.innerHTML=` ${convertedResponse.main.temp}°C`
  	
+		 	
+
 		 	ta.innerHTML=`<tr> <td>${convertedResponse.main.feels_like}°C
 		 	</td> <td>${convertedResponse.main.humidity}%
 		 	</td><td>${convertedResponse.main.pressure}hPa </td> </tr>`
