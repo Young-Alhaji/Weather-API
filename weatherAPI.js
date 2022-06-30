c012a677c4bd1b7a82024634e6a6393c3d6659c8
Ridwan <106625392+Young-Alhaji@users.noreply.github.com>
2022-06-30 02:14:15 -0700

    Add files via upload
    Geolocation ability was added to weather page to detect current location

--- weatherAPI.js
+++ weatherAPI.js
@@ -1,4 +1,23 @@
+async function getLocation() {
+    navigator.geolocation.getCurrentPosition((position)=>{
+let url = `http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&
+lon=${position.coords.longitude}&appid=09756d60b2011a99fb389f6e43e766cc&units=metric`
+fetch(url).then((response)=>response.json()).then((convertedResponse)=>{
+  locatio.innerHTML= `  ${convertedResponse.sys.country} <br> ${convertedResponse.name} `
+  locatio2.innerHTML=` ${convertedResponse.weather[0].description}`
+  maintemp.innerHTML=` ${convertedResponse.main.temp}°C`
+  
+      
 
+      ta.innerHTML=`<tr> <td>${convertedResponse.main.feels_like}°C
+      </td> <td>${convertedResponse.main.humidity}%
+      </td><td>${convertedResponse.main.pressure}hPa </td> </tr>`
+        
+
+})
+    }
+  )
+}
 
 async function forecast(){
 let cityName = cityname.value
