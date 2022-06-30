b9c56b3e3fb9e0ae1b2ba89e8d32cec64fce490b
Ridwan <106625392+Young-Alhaji@users.noreply.github.com>
2022-06-30 09:09:13 -0700

    Add files via upload

--- weatherAPI.js
+++ weatherAPI.js
@@ -1,17 +1,14 @@
 async function getLocation() {
     navigator.geolocation.getCurrentPosition((position)=>{
-let url = `http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&
+let url = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&
 lon=${position.coords.longitude}&appid=09756d60b2011a99fb389f6e43e766cc&units=metric`
 fetch(url).then((response)=>response.json()).then((convertedResponse)=>{
   locatio.innerHTML= `  ${convertedResponse.sys.country} <br> ${convertedResponse.name} `
   locatio2.innerHTML=` ${convertedResponse.weather[0].description}`
-  maintemp.innerHTML=` ${convertedResponse.main.temp}°C`
-  
-      
-
-      ta.innerHTML=`<tr> <td>${convertedResponse.main.feels_like}°C
-      </td> <td>${convertedResponse.main.humidity}%
-      </td><td>${convertedResponse.main.pressure}hPa </td> </tr>`
+  maintemp.innerHTML=` ${convertedResponse.main.temp}°C`  
+      ta.innerHTML=`${convertedResponse.main.feels_like}°C`
+      ta2.innerHTML=`${convertedResponse.main.humidity}%`
+      ta3.innerHTML=`${convertedResponse.main.pressure}hPa`
         
 
 })
@@ -21,23 +18,13 @@ fetch(url).then((response)=>response.json()).then((convertedResponse)=>{
 
 async function forecast(){
 let cityName = cityname.value
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=09756d60b2011a99fb389f6e43e766cc&units=metric`
-let response = await fetch(url)
+const url1 = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=09756d60b2011a99fb389f6e43e766cc&units=metric`
+let response = await fetch(url1)
 let convertedResponse= await response.json()
   locatio.innerHTML= `  ${convertedResponse.sys.country} <br> ${convertedResponse.name} `
   locatio2.innerHTML=` ${convertedResponse.weather[0].description}`
   maintemp.innerHTML=` ${convertedResponse.main.temp}°C`
- 	
-		 	
-
-		 	ta.innerHTML=`<tr> <td>${convertedResponse.main.feels_like}°C
-		 	</td> <td>${convertedResponse.main.humidity}%
-		 	</td><td>${convertedResponse.main.pressure}hPa </td> </tr>`
-				
-				
-				
-				
-				
-			
-			
+ 	 ta.innerHTML=`${convertedResponse.main.feels_like}°C`
+      ta2.innerHTML=`${convertedResponse.main.humidity}%`
+      ta3.innerHTML=`${convertedResponse.main.pressure}hPa`		
 	}
